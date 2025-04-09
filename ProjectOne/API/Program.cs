e6ea5766c7e426a79420834e6961b3d8cee450a8
sairamgoud42 <103407393+Sairamgoud42@users.noreply.github.com>
2025-04-09 16:48:34 +0530

    Migration Intial

--- ProjectOne/API/Program.cs
+++ ProjectOne/API/Program.cs
@@ -1,25 +1,66 @@
+using API.Extensions;
+using API.MiddleWare;
+using Core.Entities.Identity;
+using Infrastructure.Data;
+using Infrastructure.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 namespace API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddApplicationServices(builder.Configuration);
+            builder.Services.AddIdentityServices(builder.Configuration);
+            builder.Services.AddSwaggerDocumentation();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseMiddleware<ExceptionMiddleware>();
 
-            app.UseHttpsRedirection();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+            app.UseSwagger();
+            app.UseSwaggerUI();
+            app.UseSwaggerDocumentation();
 
+            app.UseStaticFiles();
+
+            app.UseCors("CorsPolicy");
+
+            app.UseAuthentication();
             app.UseAuthorization();
 
-
             app.MapControllers();
+
+            //app.UseHttpsRedirection();
+
+
+            using var scope = app.Services.CreateScope();
+            var services = scope.ServiceProvider;
+            var context = services.GetRequiredService<StoreContext>();
+            var identityContext = services.GetRequiredService<AppIdentityDbContext>();
+            var userManager = services.GetRequiredService<UserManager<AppUser>>();
+            var logger = services.GetRequiredService<ILogger<Program>>();
+            try
+            {
+                await context.Database.MigrateAsync();
+                await identityContext.Database.MigrateAsync();
+                await StoreContextSeed.SeedAsync(context);
+                await AppIdentityDbContextSeed.SeedUsersAsync(userManager);
+            }
+            catch (Exception ex)
+            {
+                logger.LogError(ex, "An error occured during migration");
+            }
+
 
             app.Run();
         }
---
+++ ProjectOne/Core/Entities/Identity/Address.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities.Identity
+{
+    public class Address
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+
+        // one-to-one relationship
+        [Required]
+        public string AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
+    }
+}
